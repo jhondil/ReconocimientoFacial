d9f9deba1a8d14d772b771d2160ec2d0937b4b8d
Jhonatan Ibarra <ibarra.jhonatann@gmail.com>
2024-05-17 16:02:09 -0500

    first commit

---
+++ SistemRecognition.py
@@ -0,0 +1,9 @@
+import cv2
+import face_recognition as fr
+import numpy as np
+import mediapipe as mp
+import os
+
+
+OutFolderPathUser="E:\PC-MIO\estudio\proyecto-python\reconocimiento facial\DataBase\Faces
+E:\PC-MIO\estudio\proyecto-python\reconocimiento facial\DataBase"
